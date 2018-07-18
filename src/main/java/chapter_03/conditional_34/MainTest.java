81d07dd9be9fec273666a943e30af4c68345e999
Administrator <Administrator@cm-PC>
2018-07-18 08:17:52 +0800

    20180718

--- src/main/java/chapter_03/conditional_34/MainTest.java
+++ src/main/java/chapter_03/conditional_34/MainTest.java
@@ -2,11 +2,13 @@ package chapter_03.conditional_34;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import chapter_03.conditional_34.service.ListService;
+
 public class MainTest {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConditionConfig.class);
-		//ListService service=context.getBean("");
-		
+		ListService service=context.getBean(ListService.class);
+		System.out.println(context.getEnvironment().getProperty("os.name")+":"+service.showListCmd());
 		context.close();
 	}
 }
